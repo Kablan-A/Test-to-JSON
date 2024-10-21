a335e48a3ddecaf252cff457358d4186b5023400
Kablan Abaidullayev <abaidullayev.k@gmail.com>
2024-10-22 01:11:32 +0600

    successful manual deploy to netlify. unsuccessful deploy from repo. unable to write to files after deploy

--- functions/api.js
+++ functions/api.js
@@ -1,8 +1,22 @@
 const express = require("express");
+const serverless = require("serverless-http");
 const fs = require("fs");
-const router = express.Router();
+const { parseQnAs } = require("./parseQnAs");
+
+const app = express();
 const path = require("path");
-const { parseQnAs } = require("../util/parseQnAs");
+const router = express.Router();
+
+// Middleware to parse form data
+app.use(express.json());
+// Serve static files from the "public" folder
+app.use(express.static(path.join(__dirname, "../dist")));
+
+// Ensure the 'generated-tests' directory exists for storing files
+const GEN_TESTS_DIR = path.join(__dirname, "../generated-tests");
+if (!fs.existsSync(GEN_TESTS_DIR)) {
+  fs.mkdirSync(GEN_TESTS_DIR);
+}
 
 // Set up a route to serve all generated tests
 router.get("/", (req, res) => {
@@ -23,7 +37,7 @@ router.get("/", (req, res) => {
       .filter((file) => file.endsWith(".json")) // Only keep .txt files
       .map((file) => ({
         name: path.basename(file, ".json"), // Remove the .txt extension for the name
-        downloadLink: `api/test-to-json/${path.basename(file)}`, // Set download link
+        downloadLink: `/.netlify/functions/api/${path.basename(file)}`, // Set download link
       }));
 
     // Respond with the list of tests
@@ -79,7 +93,7 @@ router.post("/", async (req, res) => {
     });
     return res.send({
       message: "Test generated successfully.",
-      downloadLink: `api/test-to-json/${filename}`,
+      downloadLink: `/.netlify/functions/api/${filename}`,
     });
   } catch (error) {
     // Catch any other unexpected errors
@@ -88,5 +102,11 @@ router.post("/", async (req, res) => {
   }
 });
 
-// export the router module so that server.js file can use it
-module.exports = router;
+app.use("/.netlify/functions/api", router);
+
+// For incorrect requests
+app.use((req, res) => {
+  res.status(404).send("<h1>404: This page doesn't exit</h1>");
+});
+
+module.exports.handler = serverless(app);
