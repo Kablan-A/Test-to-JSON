86f27c538c73d5b975ae135df6e23e16a72d197e
Kablan Abaidullayev <abaidullayev.k@gmail.com>
2024-10-22 00:37:29 +0600

    Revert "deploy to netlify"
    This reverts commit 5eff06b429fbb71a37cb30e6bc4b202ded15c568.

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const express = require("express");
-const serverless = require("serverless-http");
 const fs = require("fs");
 
 const app = express();
@@ -10,20 +9,17 @@ const path = require("path");
 // Middleware to parse form data
 app.use(express.json());
 // Serve static files from the "public" folder
-app.use(express.static(path.join(__dirname, "../../public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Ensure the 'generated-tests' directory exists for storing files
-const GEN_TESTS_DIR = path.join(__dirname, "../../generated-tests");
+const GEN_TESTS_DIR = path.join(__dirname, "generated-tests");
 if (!fs.existsSync(GEN_TESTS_DIR)) {
   fs.mkdirSync(GEN_TESTS_DIR);
 }
 
 // 'import' tests module from routes. it has endpoints for tests
 // All endpoints start with '/tests'
-const testToJson = require(path.join(
-  __dirname,
-  "../../routes/test-to-json.js"
-));
+const testToJson = require(path.join(__dirname, "./routes/test-to-json.js"));
 app.use("/api/test-to-json", testToJson);
 
 // For incorrect requests
@@ -35,6 +31,3 @@ app.use((req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-const handler = serverless(app);
-module.exports = { handler };
